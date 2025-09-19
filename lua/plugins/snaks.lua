98e43585148e4f94995e1f8da2919c65a7194f36
Vitalii Kohut <vkohut@intelliarts.com>
2025-09-19 11:06:52 +0300

    fix: disable animation

--- lua/plugins/snaks.lua
+++ lua/plugins/snaks.lua
@@ -4,5 +4,7 @@ return {
     dashboard = { enabled = true },
     indent = { enabled = true },
     notifier = { enabled = true },
+    scroll = { enabled = false },
+    animate = { enabled = false },
   },
 }
